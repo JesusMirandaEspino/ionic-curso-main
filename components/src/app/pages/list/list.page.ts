4714bab30429b13418d01aa8e82f921c078ee3ae
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2022-01-28 09:02:44 -0600

    get data services to ngfor

--- components/src/app/pages/list/list.page.ts
+++ components/src/app/pages/list/list.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPage implements OnInit {
 
-  constructor() { }
+  usuarios: Observable<any>;
+
+  constructor( private dataService: DataService) {
+
+
+  }
 
   ngOnInit() {
+    this.usuarios =  this.dataService.getUsuario();
   }
 
 }
