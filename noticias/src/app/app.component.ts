c666cdd92bc28fd58a3de8f9d2d3b43d4fa0082f
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-03-24 21:28:09 -0600

    appNoticias

---
+++ noticias/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+
+// 84d43b54fd294df7be96cb1309b8cab8
+
+  constructor() {}
+}
